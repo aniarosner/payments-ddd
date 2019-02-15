e08a4e42582cea49f4f0d814417e7994a19d2282
Anna Rosner <ania.rosner@gmail.com>
2019-02-15 12:07:26 +0100

    Rename attribute in OrderReference vo

--- payments/lib/payments/value_objects/order_reference.rb
+++ payments/lib/payments/value_objects/order_reference.rb
@@ -2,20 +2,20 @@ module Payments
   class OrderReference
     include Comparable
 
-    def initialize(value)
-      @value = value
+    def initialize(order_id)
+      @order_id = order_id
     end
 
     def <=>(other)
-      self.class == other.class && value == other.value ? 0 : -1
+      self.class == other.class && order_id == other.order_id ? 0 : -1
     end
 
     def to_s
-      value
+      order_id
     end
 
     protected
 
-    attr_reader :value
+    attr_reader :order_id
   end
 end
