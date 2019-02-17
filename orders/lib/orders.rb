bbe4f3cccbbcfcfb817a635d382b6a132514c967
Anna Rosner <ania.rosner@gmail.com>
2019-02-17 17:26:46 +0100

    Add methods for state validation in order bc

--- orders/lib/orders.rb
+++ orders/lib/orders.rb
@@ -1,2 +1,21 @@
 module Orders
 end
+
+require_dependency 'orders/commands/cancel_order.rb'
+require_dependency 'orders/commands/place_order.rb'
+require_dependency 'orders/commands/provide_contact_info.rb'
+require_dependency 'orders/commands/provide_shipping_info.rb'
+require_dependency 'orders/commands/submit_order.rb'
+
+require_dependency 'orders/events/contanct_info_provided.rb'
+require_dependency 'orders/events/order_cancelled.rb'
+require_dependency 'orders/events/order_placed.rb'
+require_dependency 'orders/events/order_submitted.rb'
+require_dependency 'orders/events/shipping_info_provided.rb'
+
+require_dependency 'orders/value_objects/contact_info.rb'
+require_dependency 'orders/value_objects/order_state.rb'
+require_dependency 'orders/value_objects/shipping_info.rb'
+
+require_dependency 'orders/order_command_handler.rb'
+require_dependency 'orders/order.rb'
--- orders/lib/orders/value_objects/order_state.rb
+++ orders/lib/orders/value_objects/order_state.rb
@@ -8,8 +8,32 @@ module Orders
       state == :initialized
     end
 
+    def placed?
+      state == :placed
+    end
+
+    def cancelled?
+      state == :cancelled
+    end
+
     def valid_for_place?
       state.initialized?
+    end
+
+    def valid_for_shipping_info_providing?
+      state.placed?
+    end
+
+    def valid_for_contact_info_providing?
+      state.placed?
+    end
+
+    def valid_for_submit?
+      state.placed?
+    end
+
+    def valid_for_cancel?
+      state.placed? || state.submitted?
     end
   end
 end
