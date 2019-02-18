21b98ff27cce56de9b09344ae633ed554324662e
Anna Rosner <ania.rosner@gmail.com>
2019-02-18 13:14:31 +0100

    Add order state in Fulfillment bc

---
+++ fulfillment/lib/fulfillment/value_objects/order_state.rb
@@ -0,0 +1,19 @@
+module Orders
+  class OrderState
+    def initialize(state)
+      @state = state
+    end
+
+    def initialized?
+      state == :initialized
+    end
+
+    def accepted?
+      state == :accepted
+    end
+
+    def rejeceted?
+      state == :rejeceted
+    end
+  end
+end
