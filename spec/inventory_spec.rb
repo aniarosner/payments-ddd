b9b6f5bd5a9a7022dfa91d57187f120c534a7a3b
Anna Rosner <ania.rosner@gmail.com>
2019-02-19 23:15:26 +0100

    Rename attribute method in inventory specs

--- spec/inventory_spec.rb
+++ spec/inventory_spec.rb
@@ -6,47 +6,47 @@ RSpec.describe 'Maintaining inventory' do
   it do
     command_bus.call(
       Inventory::RegisterProduct.new(
-        product_id: nice_stapler_product.product_id,
-        sku: nice_stapler_product.sku,
-        name: nice_stapler_product.name
+        product_id: nice_stapler.product_id,
+        sku: nice_stapler.sku,
+        name: nice_stapler.name
       )
     )
 
     expect(Fulfillment::InventoryReadModel.new.all.count).to eq(1)
-    expect(Fulfillment::InventoryReadModel.new.all.first.product_id).to eq(nice_stapler_product.product_id)
-    expect(Fulfillment::InventoryReadModel.new.all.first.sku).to eq(nice_stapler_product.sku)
-    expect(Fulfillment::InventoryReadModel.new.all.first.name).to eq(nice_stapler_product.name)
-    expect(Fulfillment::InventoryReadModel.new.product_quantity(nice_stapler_product.product_id)).to eq(0)
+    expect(Fulfillment::InventoryReadModel.new.all.first.product_id).to eq(nice_stapler.product_id)
+    expect(Fulfillment::InventoryReadModel.new.all.first.sku).to eq(nice_stapler.sku)
+    expect(Fulfillment::InventoryReadModel.new.all.first.name).to eq(nice_stapler.name)
+    expect(Fulfillment::InventoryReadModel.new.product_quantity(nice_stapler.product_id)).to eq(0)
 
     command_bus.call(
       Inventory::SetProductQuantity.new(
-        product_id: nice_stapler_product.product_id,
+        product_id: nice_stapler.product_id,
         quantity: 10
       )
     )
 
-    expect(Fulfillment::InventoryReadModel.new.product_quantity(nice_stapler_product.product_id)).to eq(10)
+    expect(Fulfillment::InventoryReadModel.new.product_quantity(nice_stapler.product_id)).to eq(10)
 
     command_bus.call(
       Inventory::IncreaseProductQuantity.new(
-        product_id: nice_stapler_product.product_id,
+        product_id: nice_stapler.product_id,
         quantity: 1
       )
     )
 
-    expect(Fulfillment::InventoryReadModel.new.product_quantity(nice_stapler_product.product_id)).to eq(11)
+    expect(Fulfillment::InventoryReadModel.new.product_quantity(nice_stapler.product_id)).to eq(11)
 
     command_bus.call(
       Inventory::DecreaseProductQuantity.new(
-        product_id: nice_stapler_product.product_id,
+        product_id: nice_stapler.product_id,
         quantity: 1
       )
     )
 
-    expect(Fulfillment::InventoryReadModel.new.product_quantity(nice_stapler_product.product_id)).to eq(10)
+    expect(Fulfillment::InventoryReadModel.new.product_quantity(nice_stapler.product_id)).to eq(10)
   end
 
-  def nice_stapler_product
+  def nice_stapler
     OpenStruct.new(
       product_id: '8277d4c5-37d5-4ef8-bd82-4c476e3070d2',
       sku: 'MT166-0001',
